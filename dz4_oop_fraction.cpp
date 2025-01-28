d7f1a61a9f42e6ffd9a97d0f0c2754ba2e89c9dc
Dan Kut <danakutumov@gmail.com>
2025-01-28 19:36:26 +0200

    added try catch

--- dz4_oop_fraction.cpp
+++ dz4_oop_fraction.cpp
@@ -17,21 +17,31 @@ int main()
 	int denom2;
 	cout << "Enter denominator for 2 fraction: ";
 	cin >> denom2;
+	try
+	{
+		Fraction fr1 = { num1,denom1 };
+		Fraction fr2 = { num2,denom2 };
 
-	Fraction fr1 = { num1,denom1 };
-	Fraction fr2 = { num2,denom2 };
 
-	fr1.printFract();
-	fr2.printFract();
-	
-	cout <<  fr1 + fr2 << endl;
-	cout << fr1 - fr2 << endl;
+		fr1.printFract();
+		fr2.printFract();
 
-	cout << "fr1 * fr2";
-	fr1 * fr2;
-	fr1.printFract();
-	cout << "fr1 * fr2";
-	fr1 / fr2;
-	fr1.printFract();
+		cout << fr1 + fr2 << endl;
+		cout << fr1 - fr2 << endl;
 
+		cout << "fr1 * fr2";
+		fr1* fr2;
+		fr1.printFract();
+		cout << "fr1 * fr2";
+		fr1 / fr2;
+		fr1.printFract();
+	}
+	catch (invalid_argument ex)
+	{
+		cout << ex.what() << endl;
+	}
+	catch (logic_error ex)
+	{
+		cout << ex.what() << endl;
+	}
 }
